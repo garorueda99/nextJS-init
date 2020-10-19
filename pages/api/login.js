34d1b5a96d28cde6b9f5efbdfbf1c8bf9049a87c
Andres Rueda <garo99@gmail.com>
2020-10-19 15:18:29 -0400

    login

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,8 +1,13 @@
+import { Magic } from '@magic-sdk/admin';
+
+let magic = new Magic(process.env.MAGIC_SECRET_KEY);
+
 export default async (req, res) => {
   if (req.method !== 'POST') return res.status(405).end();
 
   // exchange the DID from Magic for some user data
-  // TODO
+  const did = magic.utils.parseAuthorizationHeader(req.headers.authorization);
+  const user = await magic.users.getMetadataByToken(did);
 
   // Author a couple of cookies to persist a users session
   // TODO
