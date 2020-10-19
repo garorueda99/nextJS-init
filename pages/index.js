cc3277e41dc30cd65a5b1bf55c1ca3d36bd6cdcf
Andres Rueda <garo99@gmail.com>
2020-10-19 12:12:00 -0400

    repo test

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Test of <a href='https://nextjs.org'>Next.js!</a>
+          Testing <a href='https://nextjs.org'>Next.js!</a>
         </h1>
 
         <p className={styles.description}>
