ca212914526a26bc482cba8aaf8f6add98efee9b
Andres Rueda <garo99@gmail.com>
2020-10-30 13:08:45 -0400

    asset on DB

--- components/assetForm.js
+++ components/assetForm.js
@@ -1,23 +1,51 @@
 import styles from '../styles/AssetForm.module.css';
 export default function assetForm() {
+  const [assetInfo, setAssetInfo] = React.useState({});
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    validate();
+    console.log("HERE I'm  ==>", assetInfo);
+    try {
+      const res = await fetch('/api/assets', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'applicatrion/json',
+        },
+        body: JSON.stringify(assetInfo),
+      });
+    } catch (err) {}
+  };
+
+  const handleAssetInfoChange = (e) => {
+    setAssetInfo({
+      ...assetInfo,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const validate = () => {};
+
   return (
     <>
       <h2>New Asset Form</h2>
-      <form name='asset' method='POST' action='api/assets'>
+      <form onSubmit={handleSubmit}>
         <div>
-          <label className={styles.label}>Asset Name:</label>
-          <input />
+          <label htmlFor='name' className={styles.label}>
+            Asset Name:
+          </label>
+          <input name='name' onChange={handleAssetInfoChange} />
         </div>
         <div>
-          <label>
+          <label htmlFor='info' className={styles.label}>
             Info:
-            <input />
+            <input name='info' onChange={handleAssetInfoChange} />
           </label>
         </div>
         <div>
-          <label>
+          <label htmlFor='info2' className={styles.label}>
             Info2:
-            <input />
+            <input name='info2' onChange={handleAssetInfoChange} />
           </label>
         </div>
         <button>SAVE</button>
--- pages/api/assets/index.js
+++ pages/api/assets/index.js
@@ -1,8 +1,13 @@
+import { connectToDatabase } from '../../../lib/mongodb';
 export default async (req, res) => {
+  const { db } = await connectToDatabase();
   if (req.method !== 'POST') {
     return res
       .status(405)
       .json({ message: 'This route only accepts GET requests' });
   }
-  res.json((result: 'ok'));
+  console.log('BODY:', req.body);
+  const result = await db.collection('assets').insertOne(JSON.parse(req.body));
+  console.log('RESULT', result);
+  res.json({ result: 'ok' });
 };
